cdbe1e22daca366858cc2b834e319229f637e962
Alexandr Rodionov <alexrodionby@gmail.com>
2022-08-28 19:04:59 +0300

    init(app)start new project

---
+++ CryptoProfit/CryptoProfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoProfitApp.swift
+//  CryptoProfit
+//
+//  Created by Alexandr Rodionov on 28.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoProfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
