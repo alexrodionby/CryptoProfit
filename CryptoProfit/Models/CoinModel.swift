e78d445300b24ae62c86e923e60ad05b688ec290
Alexandr Rodionov <alexrodionby@gmail.com>
2022-09-01 17:54:54 +0300

    app(add)add service and view model to coin row, coinimageservice, some changes in networkingmanager

--- CryptoProfit/Models/CoinModel.swift
+++ CryptoProfit/Models/CoinModel.swift
@@ -6,3 +6,125 @@
 //
 
 import Foundation
+
+// MARK: CoinGecko API info. Модель монеты с www.coingecko.com
+/*
+ 
+ URL:
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ 
+ JSON Response:
+ {
+ "id": "bitcoin",
+ "symbol": "btc",
+ "name": "Bitcoin",
+ "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+ "current_price": 19839.5,
+ "market_cap": 380138279067,
+ "market_cap_rank": 1,
+ "fully_diluted_valuation": 417178113287,
+ "total_volume": 24599581545,
+ "high_24h": 20150,
+ "low_24h": 19617.3,
+ "price_change_24h": -170.19785089107245,
+ "price_change_percentage_24h": -0.85058,
+ "market_cap_change_24h": -2844895887.557556,
+ "market_cap_change_percentage_24h": -0.74283,
+ "circulating_supply": 19135481,
+ "total_supply": 21000000,
+ "max_supply": 21000000,
+ "ath": 69045,
+ "ath_change_percentage": -71.25,
+ "ath_date": "2021-11-10T14:24:11.849Z",
+ "atl": 67.81,
+ "atl_change_percentage": 29173.94677,
+ "atl_date": "2013-07-06T00:00:00.000Z",
+ "roi": null,
+ "last_updated": "2022-08-29T10:24:30.584Z",
+ "sparkline_in_7d": {
+ "price": [
+ 21455.853253963316,
+ 21358.650857679924,
+ 21192.23133210945,
+ 21144.5903334789,
+ 21191.65149834661
+ ]
+ },
+ "price_change_percentage_24h_in_currency": -0.8505769102739783
+ }
+ */
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let coin = try? newJSONDecoder().decode(Coin.self, from: jsonData)
+
+// MARK: - CoinModel
+struct CoinModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation, totalVolume: Double?
+    let high24H: Double?
+    let low24H, priceChange24H, priceChangePercentage24H, marketCapChange24H: Double?
+    let marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+    // Добавили константу для сохранения колличества монет у пользователя конкретной монеты
+    let currentHoldings: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+        case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
+        //  Не забываем добавить и сюда нашу лично добавленную константу
+        case currentHoldings
+    }
+    
+    // Функция обновления нашей текущей модели монетки, с внесение колличества монет у конкретного пользователя
+    func updateHoldings(amount: Double) -> CoinModel {
+        
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: high24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
+    }
+    
+    // Вычисляемая переменная со значением в долларах колличества конкретной монеты (стоимость всего кол-ва монет в деньгах)
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    // Возвращаем глобальный ранк монеты
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
+}
+
+struct SparklineIn7D: Codable {
+    let price: [Double]?
+}
--- CryptoProfit/Services/CoinDataService.swift
+++ CryptoProfit/Services/CoinDataService.swift
@@ -5,4 +5,29 @@
 //  Created by Alexandr Rodionov on 31.08.22.
 //
 
+// MARK: Сервис для получения всех монеток
+
 import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?   // Подписка, которую мы легко можем удалить
+    
+    init() {
+        getCoins()  // Сработает сразу на инициализации класса
+    }
+    
+    private func getCoins() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        // Делаем через привязку к отменяемой переменной паблишеру
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder()) // Декодируем в тип CoinModel декодером JSON
+            .sink(receiveCompletion: NetworkingManager.handleComplition, receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins          // Сохраняем полученные данные
+                self?.coinSubscription?.cancel()        // Отменяем подписку на паблишера
+            })
+    }
+}
